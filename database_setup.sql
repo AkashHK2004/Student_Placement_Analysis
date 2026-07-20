9afbb2362c5d15802dc87a6239108c6f1a8e7c6b
Akash H K <akashhk37@gmail.com>
2026-07-20 23:25:44 +0530

    Created students table in MySQL

---
+++ database_setup.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE placement_analysis;
+
+SHOW DATABASES;
+
+USE placement_analysis;
+
+SELECT DATABASE();
+
+CREATE TABLE students (
+    StudentID INT PRIMARY KEY,
+    CGPA DECIMAL(3,2),
+    Internships INT,
+    Projects INT,
+    Workshops_Certifications INT,
+    AptitudeTestScore INT,
+    SoftSkillsRating DECIMAL(3,2),
+    ExtracurricularActivities VARCHAR(10),
+    PlacementTraining VARCHAR(10),
+    SSC_Marks DECIMAL(5,2),
+    HSC_Marks DECIMAL(5,2),
+    PlacementStatus VARCHAR(20)
+);
+
+
+
+
